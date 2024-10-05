4716ae6b5627d8b50f828b27981be900eb28c6b1
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-05 16:47:37 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+// server/server.js
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Fallback to index.html for SPA routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
