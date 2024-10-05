29f6696033654db1f818e6eb1cbf9f3c70957125
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-05 16:33:39 -0500

    setup webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'PWA Text Editor',
+    }),
+    new WebpackPwaManifest({
+      name: 'PWA Text Editor',
+      short_name: 'TextEditor',
+      description: 'A Progressive Web App Text Editor',
+      background_color: '#ffffff',
+      theme_color: '#ffffff',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    new GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+};
